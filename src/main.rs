088cc654ff1a1f9cb45e542487a695ec0062e392
Peter Eichinger <peter.eichinger@adesso.de>
2022-09-08 16:44:11 +0200

    Wavy hexes with selection

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,91 @@
-//! This example demonstrates the built-in 3d shapes in Bevy.
-//! The scene includes a patterned texture and a rotation for visualizing the normals and UVs.
-
+mod coordinates;
+mod input;
 mod mesh_generation;
 
-use mesh_generation::hex::{create_hex, create_hex_prism};
+use bevy_rapier3d::prelude::*;
+use coordinates::{Axial, Cube, Offset};
+use input::camera_control_plugin::{CameraControlPlugin, CurrentCameraTag};
+use mesh_generation::hex::create_hex_prism;
 
-use bevy::{
-    prelude::*,
-    render::{
-        render_resource::{Extent3d, TextureDimension, TextureFormat},
-        texture::ImageSettings,
-    },
-};
+use bevy::{prelude::*, render::texture::ImageSettings};
+
+#[derive(Component)]
+struct Hexagon(Cube);
+
+#[derive(Component)]
+struct Selected;
 
 fn main() {
+    let window_descriptor = WindowDescriptor {
+        title: "bevy-hexes".into(),
+        position: WindowPosition::Centered(MonitorSelection::Number(1)),
+        ..Default::default()
+    };
+
     App::new()
+        .insert_resource(window_descriptor)
         .insert_resource(ImageSettings::default_nearest())
         .add_plugins(DefaultPlugins)
+        .add_plugin(CameraControlPlugin)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        // .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup)
-        .add_system(rotate)
+        .add_system(move_hexes)
+        .add_system(select_hex)
         .run();
 }
 
 /// A marker component for our shapes so we can query them separately from the ground plane
-#[derive(Component)]
-struct Shape;
+// #[derive(Component)]
+// struct Shape;
 
-const X_EXTENT: f32 = 14.;
+fn move_hexes(time: Res<Time>, mut query: Query<(&mut Transform, &Hexagon)>) {
+    let origin = Default::default();
+    let startup_time = 1.5 * time.time_since_startup().as_secs_f32();
+    for (mut transform, hex) in query.iter_mut() {
+        let distance = hex.0.distance_to(origin) as f32;
+
+        transform.translation.y = 1. + 0.25 * (distance + -startup_time).sin();
+    }
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
+    let material = materials.add(StandardMaterial {
+        // base_color_texture: Some(images.add(uv_debug_texture())),
+        base_color: Color::rgb_u8(0, 98, 105),
+        metallic: 0.8,
         ..default()
     });
 
-    let shapes = [
-        meshes.add(shape::Box::default().into()),
-        meshes.add(shape::Box::default().into()),
-        meshes.add(create_hex(1.0).into()),
-        meshes.add(create_hex_prism(2.0, 1.0).into()),
-    ];
+    let mesh = meshes.add(create_hex_prism(0.49, 0.25).into());
 
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        commands
-            .spawn_bundle(PbrBundle {
-                mesh: shape,
-                material: debug_material.clone(),
-                transform: Transform {
-                    translation: Vec3::new(
-                        -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                        2.0,
-                        0.0,
-                    ),
-
-                    // rotation: Quat::from_rotation_x(-std::f32::consts::PI / 4.),
+    for x in -25..=25 {
+        for y in -25..=25 {
+            let axial: Axial = Offset::new(x, y).into();
+            commands
+                .spawn_bundle(PbrBundle {
+                    mesh: mesh.clone(),
+                    material: material.clone(),
+                    transform: Transform {
+                        translation: axial.into(),
+                        ..default()
+                    },
                     ..default()
-                },
-                ..default()
-            })
-            .insert(Shape);
+                })
+                .insert(Hexagon(axial.into()))
+                .insert(RigidBody::KinematicPositionBased)
+                .insert(
+                    Collider::from_bevy_mesh(
+                        Assets::get(&meshes, &mesh).unwrap(),
+                        &ComputedColliderShape::TriMesh,
+                    )
+                    .unwrap(),
+                );
+        }
     }
 
     commands.spawn_bundle(PointLightBundle {
@@ -75,53 +95,57 @@ fn setup(
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        transform: Transform::from_xyz(20., 16.0, 0.),
         ..default()
     });
 
     // ground plane
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(shape::Plane { size: 50. }.into()),
-        material: materials.add(Color::SILVER.into()),
-        ..default()
-    });
-
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        material: materials.add(Color::GOLD.into()),
         ..default()
     });
 }
 
-fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        transform.rotate_y(time.delta_seconds() / 2.);
+fn select_hex(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    camera_query: Query<(&Transform, &Camera), With<CurrentCameraTag>>,
+    hexagon_query: Query<(Entity, &Hexagon, Option<&Selected>)>,
+) {
+    let (transform, camera) = camera_query.single();
+
+    let global_transformation = GlobalTransform::from(*transform);
+
+    let ray_origin = camera
+        .ndc_to_world(
+            &global_transformation,
+            Vec3 {
+                x: 0.,
+                y: 0.,
+                z: 1.,
+            },
+        )
+        .unwrap();
+    let ray_dir = transform.forward();
+    let max_toi = 100.;
+    let solid = true;
+    if let Some((entity, _toi)) =
+        rapier_context.cast_ray(ray_origin, ray_dir, max_toi, solid, Default::default())
+    {
+        if let Some((entity, hex, selected)) = hexagon_query
+            .iter()
+            .find(|(ent, _hex, _selected)| ent.id() == entity.id())
+        {
+            let coord = Offset::from(hex.0);
+            info!("{}", coord);
+
+            let mut entitycommands = commands.entity(entity);
+            if let Some(Selected) = selected {
+                entitycommands.remove::<Selected>()
+            } else {
+                entitycommands.insert(Selected)
+            };
+        }
     }
-}
-
-/// Creates a colorful test pattern
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-    )
 }
