3d340e33c93f2c618c43051324a3311f6efdd083
Peter Eichinger <peter.eichinger@adesso.de>
2022-09-08 17:06:06 +0200

    Use right mouse button to control rotation

--- src/input/camera_control_plugin.rs
+++ src/input/camera_control_plugin.rs
@@ -112,19 +112,16 @@ fn rotate_camera(
     transform.rotate(quat_left_right);
 }
 
-fn cursor_grab_system(
-    mut windows: ResMut<Windows>,
-    btn: Res<Input<MouseButton>>,
-    key: Res<Input<KeyCode>>,
-) {
+fn cursor_grab_system(mut windows: ResMut<Windows>, btn: Res<Input<MouseButton>>) {
     let window = windows.get_primary_mut().unwrap();
 
-    if btn.just_pressed(MouseButton::Left) {
+    const CONTROL_BUTTON: MouseButton = MouseButton::Right;
+    if btn.just_pressed(CONTROL_BUTTON) {
         window.set_cursor_lock_mode(true);
         window.set_cursor_visibility(false);
     }
 
-    if key.just_pressed(KeyCode::Escape) || btn.just_pressed(MouseButton::Right) {
+    if btn.just_released(CONTROL_BUTTON) {
         window.set_cursor_lock_mode(false);
         window.set_cursor_visibility(true);
     }
